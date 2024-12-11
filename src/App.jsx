520b4769e589d017e5cf530c3a54ea843e89e2a4
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-11 12:44:57 +0100

    create components and page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import HomeMovie from './pages/HomeMovie'
+import MovieDetail from './pages/MovieDetail'
+
 function App() {
-	return <></>
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route element={<AppLayout />}>
+					<Route index element={<HomeMovie />} />
+					<Route path='/movies/:id' element={<MovieDetail />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
---
+++ src/pages/HomeMovie.jsx
@@ -0,0 +1,3 @@
+export default function HomeMovie() {
+	return <div>HomeMovie</div>
+}
