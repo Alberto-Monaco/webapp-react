ba8ca56943139d2eaef080f09e82471992cd0e49
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-11 12:49:58 +0100

    complete header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
+import { NavLink } from 'react-router-dom'
+
 export default function Header() {
-	return <header>Header</header>
+	return (
+		<header className='bg-dark p-3 text-white  '>
+			<nav>
+				<NavLink to='/' className='text-white text-decoration-none'>
+					<strong>Home</strong>
+				</NavLink>
+			</nav>
+		</header>
+	)
 }
