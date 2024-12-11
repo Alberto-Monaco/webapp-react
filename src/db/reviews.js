e082bfb40c872154fb7243d7a71374c2a6131abd
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-11 13:48:06 +0100

    fix db reviews

--- src/db/reviews.js
+++ src/db/reviews.js
@@ -1,7 +1,9 @@
+import { text } from 'express'
+
 const reviews = [
-	{ id: 1, movie_id: 1, nickname: 'John Doe', rating: 5, comment: 'Great movie' },
-	{ id: 2, movie_id: 2, nickname: 'Jane Doe', rating: 4, comment: 'Good movie' },
-	{ id: 3, movie_id: 3, nickname: 'John Smith', rating: 3, comment: 'Average movie' }
+	{ id: 1, movie_id: 1, name: 'John Doe', vote: 5, text: 'Great movie' },
+	{ id: 2, movie_id: 2, name: 'Jane Doe', vote: 4, text: 'Good movie' },
+	{ id: 3, movie_id: 3, name: 'John Smith', vote: 3, text: 'Average movie' }
 ]
 
 export default reviews
