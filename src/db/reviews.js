8f1e976de9c1dc109b9ce3b2dc0b277674302b72
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-11 13:48:36 +0100

    fix db

--- src/db/reviews.js
+++ src/db/reviews.js
@@ -1,5 +1,3 @@
-import { text } from 'express'
-
 const reviews = [
 	{ id: 1, movie_id: 1, name: 'John Doe', vote: 5, text: 'Great movie' },
 	{ id: 2, movie_id: 2, name: 'Jane Doe', vote: 4, text: 'Good movie' },
