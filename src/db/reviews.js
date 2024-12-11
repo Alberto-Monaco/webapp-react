aae47714faca043378935ba3cd1859b7f3956c22
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-11 15:20:24 +0100

    fix db

--- src/db/reviews.js
+++ src/db/reviews.js
@@ -1,7 +1,10 @@
 const reviews = [
 	{ id: 1, movie_id: 1, name: 'John Doe', vote: 5, text: 'Great movie' },
 	{ id: 2, movie_id: 2, name: 'Jane Doe', vote: 4, text: 'Good movie' },
-	{ id: 3, movie_id: 3, name: 'John Smith', vote: 3, text: 'Average movie' }
+	{ id: 3, movie_id: 3, name: 'John Smith', vote: 3, text: 'Average movie' },
+	{ id: 4, movie_id: 1, name: 'Mike Gianni', vote: 5, text: 'medio film' },
+	{ id: 5, movie_id: 2, name: 'Gianni Doe', vote: 4, text: 'pessimo film' },
+	{ id: 6, movie_id: 3, name: 'Michael Furbo', vote: 3, text: 'Bellissimo film' }
 ]
 
 export default reviews
