827f88acf5afd43ce748c8d4bff515ee1c2a2c1c
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-12 12:00:50 +0100

    fix detailmovie and css

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -25,6 +25,7 @@ export default function MovieDetail() {
 	return (
 		<>
 			<section className='container mt-5'>
+				<h3 className='text-white '>Movie</h3>
 				<div className='card shadow-sm mb-4'>
 					<div className='card-body'>
 						<h2 className='card-title'>{movie?.title}</h2>
@@ -36,10 +37,18 @@ export default function MovieDetail() {
 							<span className='fw-bold'>Director: </span>
 							{movie?.director}
 						</p>
+						<p className='card-text'>
+							<span className='fw-bold'>Release Year: </span>
+							{movie?.release_year}
+						</p>
+						<p className='card-text'>
+							<span className='fw-bold'>Genre: </span>
+							{movie?.genre}
+						</p>
 					</div>
 				</div>
 
-				<h3 className='mb-2'>Reviews</h3>
+				<h3 className='mb-2 text-white'>Reviews</h3>
 				<div className='row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4'>
 					{movie?.reviews.map((review) => (
 						<div className='col' key={review.id}>
