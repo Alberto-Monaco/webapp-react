81d6b193db1ed1d978cb10bbd8bfd9f394e3f7b0
Alberto Monaco <alberto.monaco84@gmail.com>
2024-12-16 12:56:44 +0100

    add context in homemovie

--- src/pages/HomeMovie.jsx
+++ src/pages/HomeMovie.jsx
@@ -1,27 +1,39 @@
 import MovieCard from '../components/MovieCard'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
+import GlobalContext from '../contexts/GlobalContext'
+import Loader from '../components/Loader'
 
 export default function HomeMovie() {
 	const baseUrl = 'http://localhost:3000'
 	const [movies, setMovies] = useState([])
+	const { loading, setLoading } = useContext(GlobalContext)
 
 	useEffect(() => {
+		setLoading(true)
 		fetch(`${baseUrl}/movies`)
 			.then((res) => res.json())
-			.then((data) => setMovies(data.movies))
-
+			.then((data) => {
+				setMovies(data.movies)
+				setLoading(false)
+			})
 			.catch((err) => console.log(err))
 	}, [])
 
 	return (
-		<div className='container my-5 '>
-			<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
-				{movies.map((movie) => (
-					<div className='col' key={movie.id}>
-						<MovieCard movie={movie} />
+		<>
+			{loading ? (
+				<Loader />
+			) : (
+				<div className='container my-5 '>
+					<div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
+						{movies.map((movie) => (
+							<div className='col' key={movie.id}>
+								<MovieCard movie={movie} />
+							</div>
+						))}
 					</div>
-				))}
-			</div>
-		</div>
+				</div>
+			)}
+		</>
 	)
 }
